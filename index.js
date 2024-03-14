9644be6c5400542fd5b31cf8b495fe4a813c9f9a
Dhruv <dhruvgopani8@gmail.com>
2024-03-14 19:34:33 +0530

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs=require('fs');
+const express=require('express');
+const mongoose=require('mongoose')
+const app=express();
+const path=require('path');
+
+
+const dotenv=require('dotenv')
+const { default: Axios } = require('axios');
+
+dotenv.config({path:'./config.env'});
+
+
+
+
+let arr=["https://agnee-enterprise.onrender.com/api/test"]
+
+const getApi=async()=>{
+        arr.forEach(async (link)=>{
+            const res=await Axios.get(link);
+            console.log(res.data)
+        })
+}
+
+setInterval(async()=>{
+    await getApi();
+},600000)
+const port=process.env.PORT ||3000;
+
+const server=app.listen(port,()=>{
+    console.log(`server is listing on port ${port}`);
+})
